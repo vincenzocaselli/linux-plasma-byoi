04b614566592b8f499af25e7edb37024561a04ad
vincenzo <vincenzo@vincenzo-virtualbox>
2022-02-13 08:55:34 +0100

    added comments and other changes in custom script

--- etc/skel/.config/post-installation-scripts.sh
+++ etc/skel/.config/post-installation-scripts.sh
@@ -1,4 +1,12 @@
-sudo mkdir /tmp/aaaaaaaaaaaaaaa
+#set OpenDesktop-Dark as default KDE Plasma Global Theme
+sudo sed -i 's/LookAndFeelPackage=org.kde.breezetwilight-arcolinux.desktop/LookAndFeelPackage=OpenDesktop-Dark/g' /etc/skel/.config/kdeglobals
+
+#set Qogir-dark style as Plasma style 
+sudo sed -i 's/name=Breeze-dark/name=Qogir-dark/g' /etc/skel/.config/plasmarc
+
+#find and remove pager from bottom bar config
+#find line number where pager is located
 l2="$(grep -n "plugin=org.kde.plasma.pager" /etc/skel/.config/plasma-org.kde.plasma.desktop-appletsrc | head -n 1 | cut -d: -f1)"
+#calc and remove this line as well as the previous two lines
 l1=$((l2-2))
 sed -i.bak -e ${l1}','${l2}'d' /etc/skel/.config/plasma-org.kde.plasma.desktop-appletsrc 
