c9539792e8af4c0a6c21ab1d7dfbf9ebade36753
vincenzo <vincenzo@vincenzo-virtualbox>
2022-02-15 23:29:41 +0100

    Moved all customizations in pkg1.install

--- etc/skel/.config/post-installation-scripts.sh
+++ etc/skel/.config/post-installation-scripts.sh
@@ -1,8 +1,16 @@
 #set OpenDesktop-Dark as default KDE Plasma Global Theme
 sudo sed -i 's/LookAndFeelPackage=org.kde.breezetwilight-arcolinux.desktop/LookAndFeelPackage=OpenDesktop-Dark/g' /etc/skel/.config/kdeglobals
 
+#set OpenDesktop-DarkIcons as default KDE Plasma Icon Theme
+sudo sed -i 's/Theme=Surfn-Plasma-Dark/Theme=OpenDesktop-DarkIcons/g' /etc/skel/.config/kdeglobals
+
 #set Qogir-dark style as Plasma style 
 sudo sed -i 's/name=breeze-dark/name=Qogir-dark/g' /etc/skel/.config/plasmarc
+
+#remove taskmanager (running application bottom bar section) with spacer (to keep the rest of bottom bar content on the right side)
+sudo sed -i 's/plugin=org.kde.plasma.taskmanager/plugin=org.kde.plasma.panelspacer/g' /etc/skel/.config/plasma-org.kde.plasma.desktop-appletsrc
+#remove kde.plasma.kicker (launcher) with (another) spacer
+sudo sed -i 's/plugin=org.kde.plasma.kicker/plugin=org.kde.plasma.panelspacer/g' /etc/skel/.config/plasma-org.kde.plasma.desktop-appletsrc
 
 #find and remove pager from bottom bar config
 #find line number where pager is located
